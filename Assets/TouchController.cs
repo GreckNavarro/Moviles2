cd5759967d3784a9d8f280c550b4e5fed061fdf6
GreckNavarro <130480169+GreckNavarro@users.noreply.github.com>
2024-03-19 17:07:03 -0500

    NewSystemInputTouch

---
+++ Assets/TouchController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TouchController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTouch(InputAction.CallbackContext context)
+    {
+        switch (context.phase)
+        {
+            case InputActionPhase.Waiting:
+                Debug.Log("Waiting");
+                break;
+            case InputActionPhase.Disabled:
+                Debug.Log("Disabled");
+                break;
+            case InputActionPhase.Started:
+                Debug.Log("Started");
+                break;
+            case InputActionPhase.Performed:
+                Debug.Log("Performed");
+                break;
+            case InputActionPhase.Canceled:
+                Debug.Log("Canceled");
+                break;
+        }
+    }
+
+}
